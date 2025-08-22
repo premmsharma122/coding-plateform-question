d2f6e8fc5ac7342d4f01af2ce403a0d12295490e
premm <sonusharma4237@gmail.com>
2025-08-22 12:20:38 +0530

    Update GFG-Partition Equal Subset Sum.java (dp + rec )

--- GFG-Partition Equal Subset Sum.java
+++ GFG-Partition Equal Subset Sum.java
@@ -15,3 +15,36 @@ class Solution {
         return help(arr,trg, 0);
     }
 }
+// >>>>>>>>>> DP <<<<<<<<<<
+class Solution {
+    static boolean equalPartition(int arr[]) {
+        // code here
+        int sum =0;
+        int n = arr.length;
+
+        for(int nm : arr){
+            sum += nm;
+        }
+    
+        if(sum % 2 != 0) return false;
+        int trg = sum/2;
+        boolean dp[][] = new boolean[arr.length+1][trg+1];
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
+        }
+        for(int j=1; j<=trg; j++){
+            dp[0][j] = false;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=trg; j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][trg];
+    }
+}
