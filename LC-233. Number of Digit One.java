ef8c019ba92b1276e68ce78821df73e4f25cced3
premm <sonusharma4237@gmail.com>
2025-12-29 12:16:01 +0530

    Create LC-233. Number of Digit One.java

---
+++ LC-233. Number of Digit One.java
@@ -0,0 +1,19 @@
+public int countDigitOne(int n) {
+
+    if (n <= 0) return 0;
+    int q = n, x = 1, ans = 0;
+    do {
+        int digit = q % 10;
+        q /= 10;
+        ans += q * x;
+        if (digit == 1) ans += n % x + 1;
+        if (digit >  1) ans += x;
+        x *= 10;
+    } while (q > 0);
+    return ans;
+
+}
+
+// 40 / 40 test cases passed.
+// Status: Accepted
+// Runtime: 0 ms
