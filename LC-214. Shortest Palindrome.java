6259638dd8d77029e233996e221b6ef937944b57
premm <sonusharma4237@gmail.com>
2026-01-02 13:19:38 +0530

    Create LC-214. Shortest Palindrome.java

---
+++ LC-214. Shortest Palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        String rev = sb.reverse().toString();
+        for(int i=0; i<s.length(); i++){
+            if(s.substring(0,s.length()-i).equals(rev.substring(i))){
+                return rev.substring(0,i)+s;
+            }
+        }
+        return "";
+    }
+}
