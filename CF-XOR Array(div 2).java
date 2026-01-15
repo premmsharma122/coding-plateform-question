ba45ac43115e034820d587333f8f5e5e57ba318c
premm <sonusharma4237@gmail.com>
2026-01-15 10:28:13 +0530

    Create CF-XOR Array(div 2).java

---
+++ CF-XOR Array(div 2).java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+
+            int[] p = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                p[i] = i;
+            }
+
+            p[r] = p[l - 1];
+
+            int offset = 1 << 19;
+            for (int i = r + 1; i <= n; i++) {
+                p[i] = i + offset;
+            }
+
+            for (int i = 1; i <= n; i++) {
+                sb.append(p[i] ^ p[i - 1]).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
