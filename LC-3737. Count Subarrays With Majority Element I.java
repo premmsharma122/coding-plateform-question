253dac9bba1a87005d093c746f801837c81cadb6
premm <sonusharma4237@gmail.com>
2026-01-21 18:29:20 +0530

    Create LC-3737. Count Subarrays With Majority Element I.java

---
+++ LC-3737. Count Subarrays With Majority Element I.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int countMajoritySubarrays(int[] nums, int target) {
+
+        int n = nums.length;
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+
+            int cnt = 0;   // count of target
+
+            for(int j = i; j < n; j++){
+
+                if(nums[j] == target)
+                    cnt++;
+
+                int len = j - i + 1;
+
+                if(cnt > len / 2)
+                    ans++;
+            }
+        }
+
+        return ans;
+    }
+}
