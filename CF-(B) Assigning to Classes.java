a51c7370f98274b14a62fc91196babded17270ec
premm <sonusharma4237@gmail.com>
2026-02-03 23:44:29 +0530

    Fix input reading for variable 'n' in main method

--- CF-(B) Assigning to Classes.java
+++ CF-(B) Assigning to Classes.java
@@ -4,7 +4,7 @@ public class Main{
         Scanner s = new Scanner(System.in);
         int t=s.nextInt();
         while(t-->0){
-            int n = s.nextInt();
+            int n = s.nextInt(); 
             int arr[] = new int[2*n];
             for(int i=0; i<2*n; i++){
                 arr[i]  = s.nextInt();
