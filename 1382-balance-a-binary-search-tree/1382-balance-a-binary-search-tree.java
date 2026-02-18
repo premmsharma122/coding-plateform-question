79087719ed036f857e61469d92b608817e675a8e
premm <sonusharma4237@gmail.com>
2026-02-18 10:56:54 +0530

    Time: 2 ms (98.69%), Space: 48.5 MB (21.01%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -14,23 +14,23 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode node, List<Integer> vals) {
-        if (node == null) return;
-        inorder(node.left, vals);
-        vals.add(node.val);
-        inorder(node.right, vals);
+    public void inorder(TreeNode node, List<Integer> val){
+        if(node==null) return;
+        inorder(node.left,val);
+        val.add(node.val);
+        inorder(node.right, val);
     }
-    public TreeNode build(List<Integer> vals, int l, int r) {
-        if (l > r) return null;
-        int mid = (l + r) / 2;
-        TreeNode node = new TreeNode(vals.get(mid));
-        node.left  = build(vals, l, mid - 1);
-        node.right = build(vals, mid + 1, r);
-        return node;
+    public TreeNode build(List<Integer> val, int l , int r){
+        if(l>r) return null;
+        int mid = (l+r)/2;
+        TreeNode rt = new TreeNode(val.get(mid));
+        rt.left= build(val,l,mid-1);
+        rt.right = build(val,mid+1,r);
+        return rt;
     }
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> vals = new ArrayList<>();
-        inorder(root, vals);
-        return build(vals, 0, vals.size() - 1);
+        List<Integer> val = new ArrayList<>();
+        inorder(root,val);
+        return build(val,0,val.size()-1);
     }
 }
