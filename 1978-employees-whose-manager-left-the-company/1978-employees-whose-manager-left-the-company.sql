ace4eb7068726ea02eeb77a289e39b539267b42b
premm <sonusharma4237@gmail.com>
2026-06-23 20:33:55 +0530

    Time: 433 ms (9.26%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,10 @@
+select e.employee_id
+from Employees e
+where e.salary < 30000
+  and e.manager_id is not null
+  and e.manager_id not in
+(
+    select employee_id
+    from Employees
+)
+order by employee_id;
