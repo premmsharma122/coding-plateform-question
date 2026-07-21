633c67df841e2f92d0cbc4ced3e7531ba8746925
premm <sonusharma4237@gmail.com>
2026-07-21 22:47:49 +0530

    Time: 37 ms (10.25%), Space: 47.3 MB (15.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,25 @@
 class Solution {
-   
-    public int help(int arr[], int trg, int idx, int dp[][]){
-      if(trg == 0) {
-        return 0;
-      }
-      if(trg < 0 || idx == arr.length) return Integer.MAX_VALUE;
-      if(dp[idx][trg] != -1) return dp[idx][trg];
+    private int dp[][];
+   public int help(int idx, int arr[], int trg) {
+    if (trg == 0) return 0;
+    if (idx >= arr.length || trg < 0) return Integer.MAX_VALUE;
 
-      int ans1 = help(arr,trg-arr[idx], idx,dp);
+    if (dp[idx][trg] != -1) return dp[idx][trg];
 
-      if(ans1 != Integer.MAX_VALUE) ans1+=1;
+    int take = help(idx, arr, trg - arr[idx]);
+    if (take != Integer.MAX_VALUE)
+        take++;
 
-      int ans2 = help(arr,trg,idx+1,dp);
-      return dp[idx][trg] = Math.min(ans1, ans2);
-    }
+    int not = help(idx + 1, arr, trg);
+
+    return dp[idx][trg] = Math.min(take, not);
+}
     public int coinChange(int[] coins, int amount) {
-        
-        if(amount==0) return 0;
-        int dp[][] = new int[coins.length+1][amount+1];
-        for(int i=0; i<dp.length; i++){
+        dp = new int[coins.length+1][amount+1];
+        for(int i=0; i<=coins.length; i++){
             Arrays.fill(dp[i],-1);
         }
-        int res =  help(coins,amount,0,dp);
-        return res==Integer.MAX_VALUE ? -1 : res;
+         int ans = help(0, coins, amount);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
